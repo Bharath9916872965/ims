14031df7120de0950d4f967d222e2469f1fae154
repositoryprimgradus <repository.primgradus@gmail.com>
2024-10-30 07:50:11 +0530

    Adding Keycloak support

--- src/main/java/com/vts/ims/login/LoginDetailsServiceImpl.java
+++ src/main/java/com/vts/ims/login/LoginDetailsServiceImpl.java
@@ -13,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.HashSet;
 import java.util.Set;
 
-@Service
+//@Service
 public class LoginDetailsServiceImpl implements UserDetailsService{
 	
 	
